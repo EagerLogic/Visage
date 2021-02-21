61bda22e441425c2fbe5aacfb3cd7f13acf49eed
David Ipacs <david.ipacs@gmail.com>
2021-02-21 16:52:37 +0100

    UrlMatcher fixed

--- src/visage/src/jsMain/kotlin/visage/core/Router.kt
+++ src/visage/src/jsMain/kotlin/visage/core/Router.kt
@@ -70,12 +70,12 @@ object UrlMatcher {
         val delimiters: List<String> = listOf("\\{", "\\}")
         val specialCharRegex = """[\\/\\\\\^\\+\\.\\?\\(\\)]""".toRegex()
         val tokenRegex = "${delimiters[0]}([^${delimiters.joinToString("")}]+)${delimiters[1]}".toRegex()
-        val tokens = tokenRegex.findAll(url).map {it.value}.toList()
+        val tokens = tokenRegex.findAll(template).map {it.value}.toList()
         val variables = tokens.map { it.replace("[${delimiters.joinToString("")}]".toRegex(), "") }
 
-        var templateString = url.replace(specialCharRegex) { "\\${it.value}" }
+        var templateString = template.replace(specialCharRegex) { "\\${it.value}" }
         tokens.forEach {
-            templateString = templateString.replace(it, "(.+)")
+            templateString = templateString.replace(it, "([^\\/]+)")
         }
         val templateRegex = templateString.toRegex()
 
@@ -94,35 +94,9 @@ object UrlMatcher {
     }
 
     fun isMatches(url: String, template: String): Boolean {
-        val pathWithoutParams = template.replace("\\{([^\\{\\}]+)\\}".toRegex(), "(.+)").replace("*", "(.+)")
+        val pathWithoutParams = template.replace("\\{([^\\{\\}]+)\\}".toRegex(), "([^\\/]+)").replace("*", "(.+)")
 
         return Regex(pathWithoutParams).matches(url)
     }
 
 }
-
-//fun String.extract(template: String, delimiters: List<String> = listOf("\\{", "\\}")): Map<String, String> {
-//    val specialCharRegex = """[\\/\\\\\^\\+\\.\\?\\(\\)]""".toRegex()
-//    val tokenRegex = "${delimiters[0]}([^${delimiters.joinToString("")}]+)${delimiters[1]}".toRegex()
-//    val tokens = tokenRegex.findAll(template).map {it.value}.toList()
-//    val variables = tokens.map { it.replace("[${delimiters.joinToString("")}]".toRegex(), "") }
-//
-//    var templateString = template.replace(specialCharRegex) { "\\${it.value}" }
-//    tokens.forEach {
-//        templateString = templateString.replace(it, "(.+)")
-//    }
-//    val templateRegex = templateString.toRegex()
-//
-//    val findResult = templateRegex.find(this)
-//    var firstSkipped = false
-//    val values = mutableListOf<String>()
-//    findResult?.groups?.forEach {
-//        if (!firstSkipped) {
-//            firstSkipped = true
-//        } else {
-//            values.add(it!!.value)
-//        }
-//    }
-//
-//    return variables.zip(values).toMap()
-//}
