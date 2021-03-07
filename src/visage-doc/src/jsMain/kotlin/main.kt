829a4def71acf7ed15bf95c3933207fded1c23e0
David Ipacs <david.ipacs@gmail.com>
2021-03-07 13:33:01 +0100

    cleanup main

--- src/visage-doc/src/jsMain/kotlin/main.kt
+++ src/visage-doc/src/jsMain/kotlin/main.kt
@@ -4,9 +4,6 @@ import kotlinx.browser.window
 import org.w3c.dom.HTMLElement
 import visage.core.Navigation
 import visage.core.Visage
-import visage.dom.b
-import visage.dom.div
-import visage.ds.components.Modal
 import visagedoc.scenes.main.MainScene
 
 fun main() {
@@ -19,7 +16,6 @@ fun main() {
 fun startVisage() {
     console.log("Visage doc site starting...")
     Visage.init(window.document.getElementById("root") as HTMLElement) {
-
         MainScene()
     }
     Navigation.onLocationChanged.addListener {
